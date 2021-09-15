fdf1cebc974eb17857e3c3957933983fb8571880
minseoksakong <msakong@bu.edu>
2021-09-16 04:49:13 +0900

    Tweaked login page

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:test/scan.dart';
 
 class login extends StatefulWidget {
   const login({Key? key}) : super(key: key);
@@ -10,6 +11,26 @@ class login extends StatefulWidget {
 }
 
 class _loginState extends State<login> {
+  var isloggedIn = false;
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    setState(() => isloggedIn = true);
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,16 +39,28 @@ class _loginState extends State<login> {
       ),
       body:
 
-
       Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text(
+              isloggedIn == false
+                  ? 'Please google log-in'
+                  : 'You are now logged in!',
+            ),
             FlatButton(
               color: Colors.grey.withOpacity(0.3),
               onPressed: signInWithGoogle,
               child: Text("Google Login"),
             ),
+      FlatButton(
+        color: Colors.grey.withOpacity(0.3),
+        onPressed: isloggedIn ? (){
+          MaterialPageRoute route = MaterialPageRoute(builder: (context) => Scan());
+          Navigator.push(context, route);
+        }: null,
+        child: Text("GO"),
+      ),
           ],
         ),
       ),
@@ -44,20 +77,6 @@ class _loginState extends State<login> {
 //   );
 //   return await FirebaseAuth.instance.signInWithCredential(credential);
 // }
-Future<UserCredential> signInWithGoogle() async {
-  // Trigger the authentication flow
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-  // Obtain the auth details from the request
-  final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
 
-  // Create a new credential
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth.accessToken,
-    idToken: googleAuth.idToken,
-  );
-
-  // Once signed in, return the UserCredential
-  return await FirebaseAuth.instance.signInWithCredential(credential);
-}
 
